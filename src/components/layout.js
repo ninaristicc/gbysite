bc65aff5fb1d42ea8e0f9c7362ce0f85c523c43b
ninaristicc <ninaristic@hotmail.com>
2019-10-23 11:17:34 -0700

    delete nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 import { red } from "ansi-colors"
 import { nonExecutableDefinitionMessage } from "graphql/validation/rules/ExecutableDefinitions"
-import Navbar from "./Navbar"
-
 const ListLink = props => (
 
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -12,7 +10,7 @@ const ListLink = props => (
 )
 export default ({ children }) => (
   <div style={{ margin: `0 auto`, maxWidth: 800, padding: `0 1rem`, backgroundColor: `#f7f5e6`, height: `150vh` }}>
-          <Navbar />
+    
     <header style={{ marginBottom: `1.5rem` }}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, textDecoration: `none` }}>
         <h3 style={{ display: `inline` }}>GetMoney</h3>
