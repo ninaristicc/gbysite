77b24ea6ce0c8ce15038c8525756b0372ee79229
ninaristicc <ninaristic@hotmail.com>
2019-10-23 16:17:28 -0700

    proba

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,5 @@
 import React from "react"
 import Header from "./Header"
-import PageTransition from 'gatsby-plugin-page-transitions';
-
 
 export default ({ children }) => (
   <div className="wrap">
